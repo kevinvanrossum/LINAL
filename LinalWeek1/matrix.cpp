332a171b9aa372891246010436ac421e25982aab
Kevin van Rossum <kevin.van.rossum@plat4mation.com>
2018-11-05 23:06:00 +0100

    created matrix class

---
+++ LinalWeek1/matrix.cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+#include "matrix.h"
+
+
+void matrix::add_vector(vector vector)
+{
+	vectors_.push_back(vector);
+}
+
+vector matrix::get_vector(int index) const
+{
+	return vectors_[index];
+}
+
+int matrix::get_vertex_count() const
+{
+	return vectors_.size();
+}
+
+void matrix::clear()
+{
+	return vectors_.clear();
+}
+
---
+++ LinalWeek1/matrix.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include <vector>
+#include "vector.h"
+
+class matrix
+{
+public:
+	void add_vector(vector vector);
+	vector get_vector(int index) const;
+	int get_vertex_count() const;
+	void clear();
+private:
+	std::vector<vector> vectors_;
+};
